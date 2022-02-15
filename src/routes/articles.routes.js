51fbaa7d43f462cf27071c7a32401ce76099ff33
TheRedMine TheredMaths <75205990+TheRedMineTheRedMine@users.noreply.github.com>
2022-02-15 14:19:09 +0100

    Update articles.routes.js

--- src/routes/articles.routes.js
+++ src/routes/articles.routes.js
@@ -2,10 +2,14 @@ import { Router } from "express";
 
 const ArticleRoutes = Router(); 
 
+// Articles
 ArticleRoutes.get('/'); // Récupération de tous les articles
-ArticleRoutes.post('/new'); // Création d'un nouvel article
 ArticleRoutes.get('/:id'); // Récupération d'un article spécifique
+
+ArticleRoutes.post('/new'); // Création d'un nouvel article
+
 ArticleRoutes.put('/:id'); // Modifier un article
+
 ArticleRoutes.delete('/:id'); // Supprimer un article
 
 export default ArticleRoutes;
