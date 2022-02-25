aa40284d7876c191c97e045055898170d3968eae
TheRedMine TheredMaths <thereminedu51@gmail.com>
2022-02-25 19:07:35 +0100

    Update
    Add error handler
    Add VERSION file and update all versions
    Update README, .gitingore, package.json and .env
    Add possibilty to read .env file

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
-import express from 'express';
-import {urlencoded, json} from 'body-parser';
+import express from 'express'
+import pkg from 'body-parser';
+import { config } from 'dotenv';
+
 import ArticleRoutes from './src/routes/articles.routes.js';
+import Errors from './src/routes/errors.routes.js';
 
 const app = express();
+const {json, urlencoded} = pkg;
 
-express.use(urlencoded({ entended: true })); // Encode l'url afin d'avoir un meilleur traitement des url contenant des caractères spéciaux
-express.use(json()); // Gère le format json
+app.use(urlencoded({ entended: true })); // Encode l'url afin d'avoir un meilleur traitement des url contenant des caractères spéciaux
+app.use(json()); // Gère le format json
 
 // Routes
-
 app.use("/api/articles", ArticleRoutes);
+app.use(Errors);
 
+config();
 app.listen(process.env.DEV_PORT);
